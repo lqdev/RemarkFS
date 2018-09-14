6eff4d378f155ff716fa596707dcae1afb7ba525
Luis Quintanilla <lqdev@outlook.com>
2018-09-13 20:45:14 -0400

    Updated HISTORY file and added built pipeline

--- HISTORY.md
+++ HISTORY.md
@@ -1,3 +1,7 @@
+# 0.0.3
+
+- Created build pipeline which cleans the build directory and creates fresh Remark presentations from files in the source directory.
+
 # 0.0.2
 
 - Allow loading and customization of source/publish directory via a `json` file.
--- app.fsx
+++ app.fsx
@@ -1,11 +1,34 @@
+#r "./packages/Fake/tools/FakeLib.dll"
+open Fake.Boot
 #load "./presentations.fsx"
 #load "./config.fsx"
 
-open System.IO
 open Presentations.Operations
 open Config
+open Fake
+open Fake.IO
+open System.IO
 
-let sourcePath = Path.Combine(__SOURCE_DIRECTORY__ + Config.getSourceDirectory)
-let files = Directory.GetFiles(sourcePath)    
+let sourcePath = Path.Combine(__SOURCE_DIRECTORY__,Config.getSourceDirectory);
+let buildPath = Path.Combine(__SOURCE_DIRECTORY__,Config.getDestionationDirectory)
 
-writePresentation files
+Target "Clean" (fun _ -> 
+    Shell.CleanDir buildPath
+)
+
+Target "Build" (fun _ -> 
+    trace "Building Presentations"
+    let files = Directory.GetFiles(sourcePath)    
+    writePresentation files
+    trace "Presentations Built"
+)
+
+Target "Default" (fun _ -> 
+    trace (sprintf "Source directory cleaned %s" sourcePath)
+)
+
+"Clean"
+    ==> "Build"
+    ==> "Default"
+
+RunTargetOrDefault "Default"
